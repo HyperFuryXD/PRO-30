c04b8e7ba09dab097386fd0ff5ee8862597d69c3
HyperFuryXD <69196456+HyperFuryXD@users.noreply.github.com>
2020-11-16 13:42:24 +0800

    Add files via upload

--- Ground.js
+++ Ground.js
@@ -1,8 +1,10 @@
-class Ground extends BaseClass {
+class Ground  {
     constructor(x,y,width,height) {
       var options = {
           isStatic: true
       }
+
+      
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
--- sketch.js
+++ sketch.js
@@ -6,40 +6,45 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-var bird, slingShot;
+var bird, slingShot,ground,ground2;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
 
 function setup(){
-    var canvas = createCanvas(1200,400);
+    var canvas = createCanvas(1200,800);
     engine = Engine.create();
     world = engine.world;
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    ground2 = new Ground(800,300,300,20);
+    platform = new Ground(150,750, 300, 170);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
+    box1 = new Box(700,720,70,70);
+    box2 = new Box(920,720,70,70);
+    box5 = new Box(770,720,70,70);
+    box6 = new Box(840,720,70,70);
+    box3 = new Box(700,760,70,70);
+    box4 = new Box(920,760,70,70);
+    box7 = new Box(840,760,70,70);
+    box8 = new Box(770,760,70,70);
+    box9 = new Box(810,680,70,70);
+
+    box10 = new Box(800,280,70,70);
+    box11 = new Box(770,280,70,70);
+    box12 = new Box(830,280,70,70);
+    box13 = new Box(785,260,70,70);
+    box14 = new Box(815,260,70,70);
+    box15 = new Box(800,240,70,70);
+
     
-    log1 = new Support(810,260,300, PI/2);
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
+    bird = new Hexagon(200,650);
+
     
-
-    log3 =  new Support(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Support(760,120,150, PI/7);
-    log5 = new Support(870,120,150, -PI/7);
-
-    bird = new Hexagon(200,50);
-
-    //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
+    slingshot = new SlingShot(bird.body,{x:200, y:470});
 }
 
 function draw(){
@@ -49,22 +54,26 @@ function draw(){
     box1.display();
     box2.display();
     ground.display();
-    
-    log1.display();
-
+    ground2.display();
     box3.display();
     box4.display();
-    
-    log3.display();
-
     box5.display();
-    log4.display();
-    log5.display();
-
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingshot.display();
+    box6.display();    
+    box7.display();
+    box8.display();
+    box9.display();
+    box10.display();
+    box11.display();
+    box12.display();
+    box13.display();
+    box14.display();
+    box15.display();
+    
+    
 }
 
 function mouseDragged(){
